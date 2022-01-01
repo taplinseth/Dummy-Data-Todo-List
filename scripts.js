427192b1524def3117ecee7caf1cd65468da3b93
taplinseth <taplinseth@gmail.com>
2021-12-31 19:03:06 -0600

    commit

--- scripts.js
+++ scripts.js
@@ -1,49 +1,65 @@
     // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }]
+
+    let arrayOfTodos = [];
+    let userTodos = [];
     
     const fetchTodos = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
-        .then( (response) => response.json())
-        .then( (json) => arrayOfTodos = json)
+            .then( (response) => response.json())
+            .then( (json) => arrayOfTodos = json);
+        userTodos = arrayOfTodos;
     }
     
     const logTodos = () => {
         console.log(arrayOfTodos)
     }
-    
-    const populateTodos = () => {
-        var ol = document.getElementById('todo-list');
 
-        for(let i = 0; i < arrayOfTodos.length; i++) {
+    const populateTodos = (todos) => {
+        let ol = document.getElementById('todo-list');
 
-            var userId = arrayOfTodos[i].userId;
-            var li = document.createElement('li');
-            ol.appendChild(li);
-
-            var id = arrayOfTodos[i].id;
-            var li = document.createElement('li');
-            ol.appendChild(li);
-
-            var title = arrayOfTodos[i].title;
-            var li = document.createElement('li');
-            li.appendChild(document.createTextNode(title));
-            ol.appendChild(li);
-
-            var completed = arrayOfTodos[i].userId;
-            var li = document.createElement('li');
-            ol.appendChild(li);
+        for(let i = 0; i < todos.length; i++) {
+            let todo = todos[i];
+            let liTodo = document.createElement('li');
+            liTodo.appendChild(document.createTextNode(JSON.stringify(todo)));
+            ol.appendChild(liTodo);
         }
+    }
+
+    let userId = "";
+    
+    const idInput = (idNum) => {
+        userId = parseInt(idNum);
+        console.log(idNum);
+    }
+
+    const showCurrentUser = () => {
+        clear();
+        userTodos = arrayOfTodos.filter(todo =>
+            todo.userId == userId);
+        populateTodos(userTodos);
+    }
+
+    const clearAll = () => {
+        document.getElementById('todo-list').innerHTML = "";
+    }
+
+    const clear = () => {
+        document.getElementById('todo-list').innerHTML = "";
+    }
+
+    const showComplete = () => {
+        clear();
+        let completed = userTodos.filter(function(todo){
+            return todo.completed === true;
+        })
+        populateTodos(completed);
+    }
+
+    const showIncomplete = () => {
+        clear();
+        let notCompleted = userTodos.filter(function(todo){
+            return todo.completed === false;
+        })
+        populateTodos(notCompleted);
     }
